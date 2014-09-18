040b93e4473a080b1cfac577a459d3416facec78
Steve Kemp <steve@steve.org.uk>
2014-09-18 18:53:12 +0100

    Added /index.html plugin.

---
+++ lib/Chronicle/Plugin/Generate/Index.pm
@@ -0,0 +1,86 @@
+
+=head1 NAME
+
+Chronicle::Plugin::Generate::Index - Generate the blog-index.
+
+=head1 DESCRIPTION
+
+This module will be invoked automatically when your site is built
+via the C<on_terminate> hook which Chronicle provides.
+
+It is responsible for creating the top-level /index.html file
+which is your blogs front-page.
+
+=cut
+
+=head1 AUTHOR
+
+Steve Kemp <steve@steve.org.uk>
+
+=cut
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014 Steve Kemp <steve@steve.org.uk>.
+
+This library is free software. You can modify and or distribute it under
+the same terms as Perl itself.
+
+=cut
+
+
+package Chronicle::Plugin::Generate::Index;
+
+use strict;
+use warnings;
+
+
+=begin doc
+
+This is a sneaky hook that outputs the /index.html file.
+
+=end doc
+
+=cut
+
+sub on_terminate
+{
+    my ( $self, $config, $dbh ) = (@_);
+
+    #
+    #  The number of posts to show on the front-page
+    #
+    my $count = $config->{ 'entry-count' } || 10;
+
+    my $recent =
+      $dbh->prepare("SELECT id FROM blog ORDER BY date DESC LIMIT 0,$count") or
+      die "Failed to find recent posts";
+
+    $recent->execute() or die "Failed to execute:" . $dbh->errstr();
+    my $id;
+    $recent->bind_columns( undef, \$id );
+
+
+    my $entries;
+
+    while ( $recent->fetch() )
+    {
+        push( @$entries, Chronicle::getBlog( $dbh, $id ) );
+    }
+    $recent->finish();
+
+
+    $config->{ 'verbose' } &&
+      print "Creating : $config->{'output'}/index.html\n";
+
+    my $c = Chronicle::load_template("index.tmpl");
+    $c->param( top     => $config->{ 'top' } );
+    $c->param( entries => $entries );
+    open( my $handle, ">:encoding(UTF-8)", "$config->{'output'}/index.html" ) or
+      die "Failed to open";
+    print $handle $c->output();
+    close($handle);
+}
+
+
+1;
