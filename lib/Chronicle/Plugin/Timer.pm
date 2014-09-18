8c4a1dd97416e48d3bce93f7e2424189293f8821
Steve Kemp <steve@steve.org.uk>
2014-09-18 19:21:10 +0100

    Added plugin to report on generation-time.

---
+++ lib/Chronicle/Plugin/Timer.pm
@@ -0,0 +1,50 @@
+
+=head1 NAME
+
+Chronicle::Plugin::Timer - Record the run-time.
+
+=head1 DESCRIPTION
+
+This module just reports the time taken to generate a blog.
+
+=cut
+
+=head1 AUTHOR
+
+Steve Kemp <steve@steve.org.uk>
+
+=cut
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014 Steve Kemp <steve@steve.org.uk>.
+
+This library is free software. You can modify and or distribute it under
+the same terms as Perl itself.
+
+=cut
+
+package Chronicle::Plugin::Timer;
+
+use strict;
+use warnings;
+
+our $start;
+
+
+sub on_initiate
+{
+    my ( $self, $config, $dbh ) = (@_);
+
+    $start = time;
+}
+
+
+sub on_terminate
+{
+    my $end  = time;
+    my $time = $end - $start;
+    print "Blog generated in $time seconds\n";
+}
+
+1;
