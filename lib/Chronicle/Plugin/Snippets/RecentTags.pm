923a7eb7a1d939e7f63a1d281a13138d430c7372
Steve Kemp <steve@steve.org.uk>
2014-09-18 19:25:19 +0100

    Added snippet for showing most recent tags.
    Then used that in the default sidebar.

---
+++ lib/Chronicle/Plugin/Snippets/RecentTags.pm
@@ -0,0 +1,82 @@
+
+=head1 NAME
+
+Chronicle::Plugin::Snippets::Tags - Generate recent tags list.
+
+=head1 DESCRIPTION
+
+This module will be invoked automatically when your site is built
+via the C<on_terminate> hook which Chronicle provides.
+
+It is responsible for creating the a data-structure to show recent
+tags.  Whether you choose to use this in your templates is up to you.
+
+=cut
+
+=head1 AUTHOR
+
+Steve Kemp <steve@steve.org.uk>
+
+=cut
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014 Steve Kemp <steve@steve.org.uk>.
+
+This library is free software. You can modify and or distribute it under
+the same terms as Perl itself.
+
+=cut
+
+
+package Chronicle::Plugin::Snippets::RecentTags;
+
+use strict;
+use warnings;
+
+
+=begin doc
+
+This is a sneaky hook that builds the data-structure, and makes it
+globally available.
+
+=end doc
+
+=cut
+
+sub on_initiate
+{
+    my ( $self, $config, $dbh ) = (@_);
+    print "OK\n";
+
+    #
+    #  The number of tags to include.
+    #
+    my $count = $config->{ 'recent-tag-count' } || 10;
+
+    my $recent =
+      $dbh->prepare("SELECT name FROM tags ORDER BY id DESC LIMIT $count") or
+      die "Failed to find recent posts";
+
+    $recent->execute() or die "Failed to execute:" . $dbh->errstr();
+    my $tag;
+    $recent->bind_columns( undef, \$tag );
+
+
+    my $entries;
+
+    while ( $recent->fetch() )
+    {
+        push( @$entries, { tag => $tag } );
+    }
+    $recent->finish();
+
+
+    #
+    #  Now we have the structure.
+    #
+    $Chronicle::GLOBAL_TEMPLATE_VARS{ "recent_tags" } = $entries;
+}
+
+
+1;
