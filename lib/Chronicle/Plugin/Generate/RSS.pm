9716f61ebc5389c38b92dc20d1ae3b44a2ef5300
Steve Kemp <steve@steve.org.uk>
2014-10-03 10:41:50 +0100

    Avoid duplication the /index.rss template.
    Given that RSS is a standard there is no need to contain
    multiple copies.  Instead we have a master-template which
    is included inline in RSS.pm.
    
    If a local theme does contain an index.rss template then
    it will still be used, but otherwise we've avoided some
    duplication.
    
    This closes #26.

--- lib/Chronicle/Plugin/Generate/RSS.pm
+++ lib/Chronicle/Plugin/Generate/RSS.pm
@@ -1,15 +1,20 @@
 
 =head1 NAME
 
-Chronicle::Plugin::Generate::RSS - Generate RSS output.
+Chronicle::Plugin::Generate::RSS - Generate a RSS feed for the blog.
 
 =head1 DESCRIPTION
 
 This module will be invoked automatically when your site is built
 via the C<on_generate> hook which Chronicle provides.
 
-It is responsible for creating the top-level /index.rss file
-which is your blogs main RSS feed.
+It is responsible for creating the top-level RSS-feed for your blog,
+which will be located at C</index.rss>.
+
+If there is a file named C<index.rss> in the currently-selected
+theme it will be used as the template for the generation of the
+feed, otherwise a default RSS-template will be used, which will
+come from this module.
 
 =cut
 
@@ -37,7 +42,10 @@ pages.  This particular plugin method is invoked I<after> any
 C<on_initiate> methods which might be present.
 
 This method is responsible for generating the RSS-feed of your
-blog site, via the theme-template C<index.rxx>.
+blog site, via the theme-provided template C<index.rss>.
+
+If there is no C<index.rss> template present in the theme then a default
+will be used.
 
 =cut
 
@@ -70,16 +78,82 @@ sub on_generate
     $config->{ 'verbose' } &&
       print "Creating : $config->{'output'}/index.rss\n";
 
+
+    #
+    #  If there is a theme-provided file then use it.
+    #
     my $c = Chronicle::load_template("index.rss");
+
+    #
+    #  If not then we're going to use our default, which is
+    # contained in the __DATA__ section of this very-module.
+    #
+    if ( !$c )
+    {
+        my $tmpl = "";
+        while ( my $line = <DATA> )
+        {
+            $tmpl .= $line;
+        }
+
+        #
+        #  If there is no template read then something weird has happened
+        #
+        return unless ( $tmpl && length($tmpl) );
+
+        #
+        #  Load the template from the given scalar-file.
+        #
+        $c = HTML::Template->new( scalarref         => \$tmpl,
+                                  open_mode         => '<:encoding(UTF-8)',
+                                  die_on_bad_params => 0,
+                                  loop_context_vars => 1,
+                                  global_vars       => 1,
+                                );
+
+        #
+        #  Annoying duplication here..
+        #
+        $c->param( blog_title => $config->{ 'blog_title' } )
+          if ( $config->{ 'blog_title' } );
+        $c->param( blog_subtitle => $config->{ 'blog_subtitle' } )
+          if ( $config->{ 'blog_subtitle' } );
+
+        #
+        #  If we have global options then set them.
+        #
+        #  Duplication is bad..
+        #
+        $c->param(%Chronicle::GLOBAL_TEMPLATE_VARS);
+
+    }
+
+
+    #
+    #  At this point we should have one of the two templates loaded,
+    # but if not .. we'll just return.
+    #
     return unless ($c);
 
+
+    #
+    #  Add the entries.
+    #
     $c->param( top => $config->{ 'top' } );
     $c->param( entries => $entries ) if ($entries);
+
+    #
+    #  Output the rendered template.
+    #
     open( my $handle, ">:encoding(UTF-8)", "$config->{'output'}/index.rss" ) or
       die "Failed to open";
     print $handle $c->output();
     close($handle);
 
+
+    #
+    #  Show the number of entries written if we're being verbose.
+    #
     if ( $config->{ 'verbose' } && $entries )
     {
         print "Wrote " . scalar(@$entries) .
@@ -109,3 +183,24 @@ b) the Perl "Artistic License".
 Steve Kemp <steve@steve.org.uk>
 
 =cut
+
+
+__DATA__
+<?xml version="1.0" encoding="utf-8"?>
+<rss version='2.0' xmlns:lj='http://www.livejournal.org/rss/lj/1.0/' xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title><!-- tmpl_var name='blog_title' escape='html' --></title>
+		<description><!-- tmpl_var name='blog_subtitle' escape='html' --></description>
+                <link><!-- tmpl_var name='top' --></link>
+                <atom:link href="<!-- tmpl_var name='top' -->index.rss" rel="self" type="application/rss+xml" />
+	<!-- tmpl_loop name="entries" -->
+	<item>
+		<title><!-- tmpl_var name="title" escape='html' --></title>
+		<link><!-- tmpl_var name='top' --><!-- tmpl_var name='link' --></link>
+		<guid isPermaLink="true"><!-- tmpl_var name='top' --><!-- tmpl_var name='link' --></guid>
+                <pubDate><!-- tmpl_var name='date' --></pubDate>
+		<description><!-- tmpl_var name="body" escape='html' --></description>
+	</item>
+	<!-- /tmpl_loop -->
+        </channel>
+</rss>
