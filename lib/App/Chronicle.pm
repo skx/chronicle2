3083748ae9f3b6cf1121467e96cf534d3f8b4b4c
Steve Kemp <steve@steve.org.uk>
2015-04-18 17:45:30 +0100

    Added

---
+++ lib/App/Chronicle.pm
@@ -0,0 +1,55 @@
+
+=head1 NAME
+
+App::Chronicle - Holder for our version number
+
+=head1 SYNOPSIS
+
+      use strict;
+      use warnings;
+
+      use App::Chronicle;
+
+      print $App::Chronicle::Version;
+
+=cut
+
+=head1 DESCRIPTION
+
+This module is a mere placeholder for our version number.
+
+=cut
+
+
+
+package App::Chronicle;
+
+use strict;
+use warnings;
+
+
+our $VERSION = "5.0.8";
+
+
+
+1;
+
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it and/or modify it
+under the terms of either:
+
+a) the GNU General Public License as published by the Free Software
+Foundation; either version 2, or (at your option) any later version,
+or
+
+b) the Perl "Artistic License".
+
+=cut
+
+=head1 AUTHOR
+
+Steve Kemp <steve@steve.org.uk>
+
+=cut
